f170a6aaf0316ee7bd7b81f4e11f9a623ea1de9c
duncanidaho14 <elhadibeddarem@gmail.com>
2020-04-30 17:23:47 +0200

    administration annonce et commentaire

--- src/Controller/AdminAnController.php
+++ src/Controller/AdminAnController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,5 +20,64 @@ class AdminAnController extends AbstractController
         return $this->render('admin/an/index.html.twig', [
             'ads' => $repo->findAll()
         ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition de la zone d'administration
+     * 
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit") 
+     * 
+     * @param Ad $ad
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée ! "
+            );
+        }
+        return $this->render('admin/an/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet de supprimer une annonce
+     * 
+     * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     *
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return void
+     **/
+    public function delete(Ad $ad, EntityManagerInterface $manager)
+    {
+        if(count($ad->getBookings()) > 0)
+        {
+            $this->addFlash(
+                'warning',
+                "Vous ne pouvez pas supprimer l'annonce <strong>{$ad->getTitle()}</strong> car elle possède déjà des réservations !"
+            );
+        } else {
+            $manager->remove($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                    'success',
+                    "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+            );
+
+        }
+        
+        return $this->redirectToRoute('admin_ads_index');
     }
 }
